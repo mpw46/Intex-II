ecc287071086451db34d6575ba0c79fe73c558ed
mpw46 <mpw46@byu.edu>
2026-04-06 20:43:42 -0600

    remove another hardcoded password

--- backend/Intex2.API/Intex2.API/Data/Intex2104Context.cs
+++ backend/Intex2.API/Intex2.API/Data/Intex2104Context.cs
@@ -49,10 +49,6 @@ public partial class Intex2104Context : DbContext
 
     public virtual DbSet<Supporter> Supporters { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:intex2.database.windows.net,1433;Initial Catalog=intex2-1-04;Persist Security Info=False;User ID=admin1-04;Password=Verysecurepassword88;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Donation>(entity =>
